1c79d931834d3e212d3e9483ab9c956c99fa1061
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-08-27 12:02:19 +0900

    12:02

--- 2_pandas/titanic.py
+++ 2_pandas/titanic.py
@@ -429,3 +429,23 @@ print('명령 기반 loc slicing\n', data_df.loc['one':'two','Name'])
 print(data_df_reset.loc[1:2, 'Name'])
 # 1     Chulmin
 # 2    Eunkyung
+
+# 불린 인덱싱 
+
+titanic_df = pd.read_csv('titanic_train.csv')
+titanic_boolean = titanic_df[titanic_df['Age']>60]
+print(type(titanic_boolean))
+print(titanic_boolean)
+
+
+print(titanic_df[titanic_df['Age']>60][['Name','Age']].head(3))
+
+
+print(titanic_df.loc[titanic_df['Age']>60,['Name','Age']].head(3))
+
+print(titanic_df[ (titanic_df['Age']>60) & (titanic_df['Pclass']==1) & (titanic_df['Sex']=='female')])
+
+cond1 = titanic_df['Age']>60
+cond2 = titanic_df['Pclass']==1
+cond3 = titanic_df['Sex']=='female'
+print(titanic_df[cond1 & cond2 & cond3])
