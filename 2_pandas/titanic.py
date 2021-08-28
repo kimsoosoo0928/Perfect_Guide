973c0803b1cd46ef82ff7d4afe3064723a697ae5
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-08-28 16:57:57 +0900

    pandas clear

--- 2_pandas/titanic.py
+++ 2_pandas/titanic.py
@@ -547,3 +547,42 @@ print(titanic_df.isna().sum())
 # Cabin          0
 # Embarked       0
 
+def get_square(a):
+        return a**2
+
+print('3의 제곱은 : ', get_square(3))
+
+lambda_square = lambda x : x ** 2
+print("3의 제곱은 : ", lambda_square(3))
+
+a = [1,2,3]
+squares = map(lambda x : x**2, a)
+
+titanic_df['Name_len'] = titanic_df['Name'].apply(lambda x : len(x))
+print(titanic_df[['Name', 'Name_len']].head(3))
+
+titanic_df['Child_Adult'] = titanic_df['Age'].apply(lambda x : 'Child' if x <= 15 else 'Adult')
+print(titanic_df[['Age', 'Child_Adult']].head(8))
+
+titanic_df['Age_cat'] = titanic_df['Age'].apply(lambda x : 'Child' if x <= 15 else ('Adult' if x <=60 else 'Elderly'))
+
+print(titanic_df['Age_cat'].value_counts())
+
+# Elderly    831
+# Adult       45
+# Child       15
+
+def get_category(age):
+        cat=''
+        if age <=5: cat='Baby'
+        elif age <=12: cat='Child'
+        elif age <=18: cat='Teenager'
+        elif age <=25: cat='Student'
+        elif age <=35: cat='Young Adult'
+        elif age <=60: cat='Adult'
+        else : cat = 'Elderly'
+
+        return cat
+
+titanic_df['Age_cat'] = titanic_df['Age'].apply(lambda x : get_category(x))
+print(titanic_df[['Age', 'Age_cat']].head())
